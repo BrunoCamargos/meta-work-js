f077c2f0ec18e30724c36cee3fb8f0657de518c1
Bruno Camargos <bruno.camargos@gmail.com>
2016-10-15 20:05:14 -0300

    Fix eslint errors

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,5 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import path from 'path';
@@ -8,19 +10,17 @@ const plugins = gulpLoadPlugins();
 
 const paths = {
   appFiles: ['./lib/**/*.js', './package.json', '!dist/**', '!node_modules/**', '!coverage/**'],
-  tests: ['./test/**/*.js']
+  tests: ['./test/**/*.js'],
 };
 
 gulp.task('clean', () =>
   del(['dist/**', 'coverage/**'])
-  // del(['dist/**', 'coverage/**', '!dist', '!coverage']) // Alternativa para manter dist e coverage
 );
 
 gulp.task('build', ['clean'], () =>
   gulp.src(paths.appFiles)
-  // .pipe(plugins.newer('dist')) // Os diretórios estão sendo excluídos
   .pipe(plugins.babel({
-    ignore: ['./package.json']
+    ignore: ['./package.json'],
   }))
   .pipe(gulp.dest('dist'))
 );
@@ -28,42 +28,29 @@ gulp.task('build', ['clean'], () =>
 gulp.task('serve', ['build'], () =>
   plugins.nodemon({
     script: path.join('dist', 'lib/index.js'),
-    tasks: ['build']
-  }) 
+    tasks: ['build'],
+  })
 );
 
 gulp.task('pre-test', ['clean'], () =>
   gulp.src([...paths.appFiles, '!*.json'])
   .pipe(plugins.istanbul({
     instrumenter: Instrumenter,
-    includeUntested: true // Inclui os arquivos sem testes
+    includeUntested: true, // Inclui os arquivos sem testes
   }))
   // Force `require` to return covered files
   .pipe(plugins.istanbul.hookRequire())
 );
 
-gulp.task('test', ['pre-test'], () => {
-  // let exitCode = 0;
-
-  return gulp.src(paths.tests, { read: false })
-    // .pipe(plugins.plumber())
-    .pipe(plugins.mocha())
-    // .once('error', (err) => {
-    //   // plugins.util.log(err);
-    //   console.log(err);
-    //   exitCode = 1;
-    // })
-    .pipe(plugins.istanbul.writeReports({
-      reporters: ['lcov', 'text', 'text-summary']
-    }))
-    .pipe(plugins.istanbul.enforceThresholds({
-      thresholds: {
-        global: { statements: 80, branches: 80, functions: 80, lines: 76 }
-      }
-    }))
-    // .once('end', () => {
-    //   // plugins.util.log('completed !!');
-    //   console.log('completed!!!')
-    //   process.exit(exitCode);
-    // });
-});
+gulp.task('test', ['pre-test'], () =>
+  gulp.src(paths.tests, { read: false })
+  .pipe(plugins.mocha())
+  .pipe(plugins.istanbul.writeReports({
+    reporters: ['lcov', 'text', 'text-summary'],
+  }))
+  .pipe(plugins.istanbul.enforceThresholds({
+    thresholds: {
+      global: { statements: 80, branches: 80, functions: 80, lines: 76 },
+    },
+  }))
+);
--- test/unit/expense-transactions-test.js
+++ test/unit/expense-transactions-test.js
@@ -1,10 +1,11 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+
 import chai from 'chai';
-import { soma } from '../../lib/expense-transactions';
+import soma from '../../lib/expense-transactions';
 
 const assert = chai.assert;
 
 describe('expense-transactions.js', () => {
-
   it('Should some 1 + 3', (done) => {
     assert.equal(soma(1, 3), 4);
     done();
